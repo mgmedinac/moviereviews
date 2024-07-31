8984bcbc8f85da6252c698f67d479afed404df50
mcmedinag <mcmedinag@eafit.edu.co>
2024-07-31 16:17:43 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
+
+# Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 
 def home(request):
-    #return HttpResponse ('<h1>Welcome to Home page</h1>')
-    #return render(request, 'home.html')
-    return render (request, 'home.html', {'name':'Maria Clara '})
-
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'name': 'Maria Clara', 'searchTerm': searchTerm, 'movies': movies})
 
 def about(request):
-        return HttpResponse ('<h1>Welcome to about page</h1>')
+    return render(request, 'home.html')
 
--- mymoviereviews/settings.py
+++ mymoviereviews/settings.py
@@ -9,12 +9,22 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Usa BASE_DIR como Path para construir las rutas
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
+# Configuración de la base de datos
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
